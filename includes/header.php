9289e063d091a84181f9c2da36818dcde516b0f3
ssgirls <ssgirlsinterschooljehanabad@gmail.com>
2025-05-19 10:44:52 +0530

    my first file

---
+++ includes/header.php
@@ -0,0 +1,48 @@
+<div class="wrapper col1">
+  <div id="header">
+    <div id="logo">
+      <img src="/home/iopsadmin/Desktop/websites/school-education/images/demo/200x140.gif" alt="" />
+      <p> <li><center><h1>SHIFA EYE CLINIC</center></h1>
+      <p> <center > <img src="images/gallery/heading.jpeg"  class="img-fluid galleryImage" alt="Patient Checking" title="IPD patient checking"></center></p>
+      <p><center>A Unit for Complete Eye Disease Solution Center</center></p></li></ul>
+    </div>
+
+
+<nav class="navbar navbar-expand-lg bg-light fixed-top shadow-lg">
+                <div class="container">
+                    
+
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav mx-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="index.php">Home</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#about">About</a>
+                            </li>
+
+                                                     
+                            <li class="nav-item">
+                                <a class="nav-link" href="check-appointment.php">Check Appointment</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#booking">Booking</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#contact">Contact</a>
+                            </li>
+                             <li class="nav-item active">
+                                <a class="nav-link" href="doctor/login.php">Doctor</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+            </nav>
